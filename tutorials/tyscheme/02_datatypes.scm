f42318c962ae9f64604922e449c1136b8186d5c6
EMacAdie <EMacAdie@gmail.com>
2016-08-09 22:40:07 -0500

    Done with chapter 2

--- tutorials/tyscheme/02_datatypes.scm
+++ tutorials/tyscheme/02_datatypes.scm
@@ -125,6 +125,81 @@ hello ;; Hello
 (string-set! hello 1 #\a)
 hello ;; Hallo
 
+;; vectors - sequances that can contain anything
+(vector 0 1 2 3 4) ;; #(0 1 2 3 4)
+(define v (make-vector 5)) ;; define a vector named "v" with 5 elements
+;; #(0 0 0 0 0)
+(vector-set! v 2 "h") ;; results in v containing #(0 0 "h" 0 0)
+(vector? v) ;; #t
+(vector? "Hello") ;; #f
+(vector-ref v 2) ;; "h"
+
+;; dotted pairs and lists
+(cons 1 #t) ;; (1 . #t)
+'(1 . #t) ;; (1 . #t)
+(define x (cons 1 #t))
+x ;; (1 . #t)
+(car x) ;; 1
+(cdr x) ;; #t
+(set-car! x 2)
+x ;; (2 . #t)
+(set-cdr! x "false")
+x ;; (2 . "false")
+(define y (cons (cons 1 2) 3))
+y ;; ((1 . 2) . 3)
+(car  (car y)) ;; 1
+(cdr (car y)) ;; 2
+(caar y) ;; 1
+(cdar y) ;; 2
+(cons 1 (cons 2 (cons 3 (cons 4 5)))) ;; (1 2 3 4 . 5)
+(list 1 2 3 4) ;; (1 2 3 4)
+(list 1 2 3 "hello") ;; (1 2 3 "hello")
+'(1 2 3 4) ;; (1 2 3 4)
+(define yy (list 1 2 3 4)) 
+yy ;; (1 2 3 4)
+(list-ref yy 0) ;; 1
+(list-ref yy 3) ;; 4
+(list-tail yy 1) ;; (2 3 4)
+(list-tail yy 3) ;; (4)
+(pair? '(1 . 2)) ;; #t
+(pair? '(1 2)) ;; #t
+(pair? '()) ;; #f
+(list? '()) ;; #t
+(null? '()) ;; #t
+(list? '(1 2)) ;; #t
+(list? '(1 . 2)) ;; #f
+(null? '(1 2)) ;; #f
+(null? '(1 . 2)) ;; #f
+
+;; conversions between types
+(char->integer #\d) ;; 100
+(integer->char 50) ;; #\2
+(string->list "hello") ;; (#\h #\e #\l #\l #\o)
+(number->string 16) ;; "16"
+(string->number "16") ;; 16
+(string->number "I love Scheme") ;; #f
+(string->number "16" 8) ;; 14
+(symbol->string 'symbol) ;; "symbol"
+
+;; other data types
+;; converting between types
+(char->integer #\d) ;; 100
+(integer->char 50) ;; #\2
+(string->list "hello") ;; (#\h #\e #\l #\l #\o)
+(number->string "16") ;; Exception in number->string: "16" is not a number.
+(number->string 16) ;; "16"
+(string->number "16") ;; 16
+(string->number "I love Scheme") ;; #f
+(string->number "16" 8) ;; 14
+(symbol->string 'symbol) ;; "symbol"
+
+;; Scheme also has procedures as a data type
+symbol->string ;; #<procedure symbol->string>
+cons ;; #<procedure cons>
+make-list ;; #<procedure make-list>
+;; I/O conduits are called "ports" in Scheme.
+;; yet another use for word "port"
+(display "Hello, World!" (current-output-port)) ;; Hello, World!
 
 
 
