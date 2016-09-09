20f31c1c6a0d1d0b437cd322fd7d6dd0710d88e7
EMacAdie <EMacAdie@gmail.com>
2016-09-09 00:34:05 -0500

    Working on lesson 3

--- tutorials/shido_yat/03_making_lists.scm
+++ tutorials/shido_yat/03_making_lists.scm
@@ -16,4 +16,23 @@
 ;; Kawa: (Sum of (1 2 3 4) is 10)
 ;; chez and guile: ("Sum of" (1 2 3 4) "is" 10)
 
+;; car is first element in a list
+(car '(1 2 3 4)) ;; 1
+;; cdr is everything else in the list
+(cdr '(1 2 3 4)) ;; (2 3 4)
+
+;; Exercise 2
+(car '(0)) ;; 0 ;; so you can take the "pair" of a 1-element list
+(cdr '(0)) ;; ()
+(car '((1 2 3) (4 5 6))) ;; (1 2 3)
+(cdr '(1 2 3 . 4)) ;; (2 3 . 4) ;; I thought it would return just 4
+(cdr (cons 3 (cons 2 (cons 1 '())))) ;; (2 1)
+
+;; lists
+(list) ;; ()
+(list 1) ;; (1)
+(list '(1 2) '(3 4)) ;; ((1 2) (3 4))
+(list 0) ;; (0)
+(list 1 2) ;; (1 2)
+
 
