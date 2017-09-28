5cf1fdfd666dd8be4627833ae171a8f579665ac6
Eric MacAdie <eric@MacAdie.net>
2017-09-27 22:18:43 -0500

    Working on chapter 8

--- tutorials/simply.scheme/chapter08.scm
+++ tutorials/simply.scheme/chapter08.scm
@@ -1,3 +1,4 @@
+;; Chapter Eight
 ;; Higher-order functions
 ;; every is like map
 ;; "every transforms each element of a word or sentence individually. The result sentence usually contains as many elements as the argument"
@@ -38,4 +39,79 @@
 
 ;; ((repeated plural 4) 'computer)
 ;; ((repeated #function #number-of-times) args-to-function)
+
+;; 8.1  What does Scheme return as the value of each of the following expressions? Figure it out for yourself before you try it on the computer.
+
+(every last '(algebra purple spaghetti tomato gnu))
+;; Guess: aeiou
+;; actual: (a e i o u)
+;; same thing, right?
+(keep number? '(one two three four))
+;; guess: empty list
+;; actual: ()
+(accumulate * '(6 7 13 0 9 42 17))
+;; guess: 0
+;; actual: 0
+(member? 'h (keep vowel? '(t h r o a t)))
+;; guess: false
+;; actual: #f
+(every square (keep even? '(87 4 7 12 0 5)))
+;; guess: list with 16 and 144
+;; actual: 16, 144, 0
+;; I did not know 0 was even
+;; I thought it was nothing
+(accumulate word (keep vowel? (every first '(and i love her))))
+;; guess: ai
+;; actual: ai
+((repeated square 0) 25)
+;; guess: 0, or empty list
+;; actual: 25 
+(every (repeated bl 2) '(good day sunshine))
+;; guess: good or go d sunshi
+;; actual: (go d sunshi)
+
+;; 8.2  Fill in the blanks in the following Scheme interactions:
+
+;; (______ vowel? 'birthday)
+;; IA
+;; my guess: keep
+(keep vowel? 'birthday)
+;; chicken gave "ia" lower case
+
+;; (______ first '(golden slumbers))
+;; (G S)
+;; my guess: every
+(every first '(golden slumbers))
+;; chicken gave lower case again (g s)
+
+;; (______ '(golden slumbers))
+GOLDEN
+
+;; (______ ______ '(little child))
+;; (E D)
+;; my guess: every last
+
+(every last '(little child))
+;; chicken gives (e d)
+
+;; (______ ______ (______ ______ '(little child)))
+;; ED
+;; my guess:
+(every se (every last '(little child)))
+;; that was wrong
+;; this worked:
+(accumulate word (every last '(little child)))
+
+ 
+;; (______ + '(2 3 4 5))
+;; (2 3 4 5)
+;; my guess: keep
+(keep + '(2 3 4 5))
+;; chicken gave (2 3 4 5)
+;; (______ + '(2 3 4 5))
+;; 14
+;; my guess: accumulate
+(accumulate + '(2 3 4 5))
+;; chicken gave 14
+
 
