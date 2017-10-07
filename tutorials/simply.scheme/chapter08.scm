38d54375233d748a1d66bb2786ce3f72f76d65ce
Eric MacAdie <eric@MacAdie.net>
2017-10-07 00:36:13 -0500

    Done with chapter 8

--- tutorials/simply.scheme/chapter08.scm
+++ tutorials/simply.scheme/chapter08.scm
@@ -313,6 +313,27 @@ GOLDEN
 ;; and returns the real phone number, in this case 7672676. 
 ;; You will need to write a helper procedure that uses an 8-way cond expression to translate a single letter into a digit.
 
+(define (phone-letter lttr)
+  (cond ((member? lttr 'abc) 2)
+        ((member? lttr 'def) 3)
+        ((member? lttr 'ghi) 4)
+        ((member? lttr 'jkl) 5)
+        ((member? lttr 'mno) 6)
+        ((member? lttr 'pqrs) 7)
+        ((member? lttr 'tuv) 8)
+        ((member? lttr 'wxyz) 9)
+        (else 0)))
 
+(define (phone-unspell wd)
+  (every phone-letter wd))
 
+;; 8.14  Write the procedure subword that takes three arguments: a word, a starting position number, and an ending position number. 
+;; It should return the subword containing only the letters between the specified positions:
+
+(subword 'polythene 5 8)
+;; THEN
+
+(define (subword wd start fin)
+  ((repeated butfirst (- start 1)) ((repeated butlast (- (count wd) fin)) wd)))
+ 
 
