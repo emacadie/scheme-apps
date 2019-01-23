c899e59b1a5993e6b616b64dc1b215e742d73c92
Eric MacAdie <eric@MacAdie.net>
2019-01-22 21:55:08 -0600

    Done with chapter 14

--- tutorials/simply.scheme/chapter14.scm
+++ tutorials/simply.scheme/chapter14.scm
@@ -273,7 +273,8 @@
   (display-all "calling accumulate-r, sent: " sent ", outp: " outp)
   (if (empty? sent)
       outp
-      (accumulate-r (bf sent) (word outp (first sent)))))
+      (accumulate-r (bf sent) (____ outp (first sent)))))
+;; the last section of the line could also be reversed: (____ (first sent) outp)
 
 ;; Exercises
 ;; Classify each of these problems as a pattern (every, keep, or accumulate), 
@@ -349,7 +350,7 @@
         (else (member-r-helper the-word (butfirst the-sent) #f))))
 
 ;; 14.7  Write differences, which takes a sentence of numbers as its argument 
-and returns a sentence containing the differences between adjacent elements. 
+;; and returns a sentence containing the differences between adjacent elements. 
 ;; (The length of the returned sentence is one less than that of the argument.)
 ;; So we do not go all the way through.
 ;; > (differences '(4 23 9 87 6 12))
@@ -369,6 +370,7 @@ and returns a sentence containing the differences between adjacent elements.
 ;; > (expand '(the 7 samurai))
 ;; (THE SAMURAI SAMURAI SAMURAI SAMURAI SAMURAI SAMURAI SAMURAI)
 ;; I will have to make a helper function, that can also be recursive.
+;; But this looks like every.
 (define (print-n-times the-num the-word outp)
   ; (display-all "calling print-n-times with the-num: " the-num ", the-word: " the-word ", outp: " outp)
   (cond ((equal? 0 the-num) outp)
@@ -378,6 +380,157 @@ and returns a sentence containing the differences between adjacent elements.
         ((number? (first the-sent)) (expand-r (butfirst (butfirst the-sent)) (sentence outp (print-n-times (first the-sent) (second the-sent) '()))))
         (else (expand-r (butfirst the-sent) (sentence outp (first the-sent))))))
 
+;; 14.9  Write a procedure called location that takes two arguments, a word and a sentence. 
+;; It should return a number indicating where in the sentence that word can be found. 
+;; If the word isn't in the sentence, return #f. 
+;; If the word appears more than once, return the location of the first appearance.
+;; > (location 'me '(you never give me your money))
+;; 4
+;; Shouldn't it return 0 if the word is not found? I don't like the idea that it returns a number OR a boolean.
+;; That kind of goes against his advice in chapter 12.
+;; Sort of like accumulate, but like member? you do not have to go all the way through.
+(define (location the-word sent)
+  (location-r the-word sent 0 1))
+(define (location-r the-word sent outp counter)
+  (display-all "calling location-r with the-word: " the-word ", sent: " sent ", outp: " outp ", counter: " counter)
+  (cond ((equal? (count sent) 0) outp)
+        ((equal? the-word (first sent)) (location-r the-word '() counter counter))
+        (else (location-r the-word (butfirst sent) outp (+ counter 1)))))
+
+;; 14.10  Write the procedure count-adjacent-duplicates that takes a sentence as an argument 
+;; and returns the number of words in the sentence that are immediately followed by the same word:
+;; > (count-adjacent-duplicates '(y a b b a d a b b a d o o))
+;; 3
+;; > (count-adjacent-duplicates '(yeah yeah yeah))
+;; 2
+;; I think this is like accumulate
+;; (count-adjacent-dups-r '(y a b b a d a b b a d o o) 0)
+(define (count-adjacent-dups-r the-sent outp)
+  (display-all "calling count-adjacent-dups-r with the-sent: " the-sent ", outp: " outp)
+  (cond ((equal? (count the-sent) 1) outp)
+        ((equal? (first the-sent) (second the-sent)) (count-adjacent-dups-r (butfirst the-sent) (+ 1 outp)))
+        (else (count-adjacent-dups-r (butfirst the-sent) outp))))
+
+;; 14.11  Write the procedure remove-adjacent-duplicates that takes a sentence as argument 
+;; and returns the same sentence but with any word that's immediately followed by the same word removed:
+;; > (remove-adjacent-duplicates '(y a b b a d a b b a d o o))
+;; (Y A B A D A B A D O)
+;; > (remove-adjacent-duplicates '(yeah yeah yeah))
+;; (YEAH)
+;; This is like keep
+;; (remove-adj-dups-r '(y a b b a d a b b a d o o) '())
+;; (remove-adj-dups-r '(yeah yeah yeah) '())
+(define (remove-adj-dups-r the-sent outp)
+  (display-all "calling remove-adj-dups-r with the-sent: " the-sent ", outp: " outp)
+  (cond ((equal? (count the-sent) 0) outp)
+        ((and (equal? (count the-sent) 1) (equal? (first the-sent) (last outp))) outp)
+        ((equal? (first the-sent) (second the-sent)) (remove-adj-dups-r (butfirst (butfirst the-sent)) (sentence outp (first the-sent))))
+        (else (remove-adj-dups-r (butfirst the-sent) (sentence outp (first the-sent))))))
+
+;; 14.12  Write a procedure progressive-squares? that takes a sentence of numbers as its argument. 
+;; It should return #t if each number (other than the first) is the square of the number before it:
+;; > (progressive-squares? '(3 9 81 6561))
+;; #T
+;; > (progressive-squares? '(25 36 49 64))
+;; #F
+;; This is accumulate
+;; (progressive-squares? '(3 9 81 6561))
+;; (progressive-squares? '(25 36 49 64))
+(define (progressive-squares? the-sent)
+  (prog-sqrs? the-sent #t))
+
+(define (prog-sqrs? the-sent outp)
+  (display-all "calling prog-sqrs? with the-sent: " the-sent ", outp: " outp)
+  (cond ((or (equal? (count the-sent) 0) (equal? (count the-sent) 1)) outp)
+        ; ((equal? (count the-sent) 0) outp)
+        ; ((equal? (count the-sent) 1) outp)
+        ; This does not work ((equal? (count the-sent) (or 1 0)) outp)
+        ((equal? (square (first the-sent)) (second the-sent)) (prog-sqrs? (butfirst the-sent) #t))
+        ;; if false, empty the-sent to get us to our base case
+        (else (prog-sqrs? '() #f))))
+
+;; 14.13  What does the pigl procedure from Chapter 11 do if you invoke it with a word like "frzzmlpt" that has no vowels? 
+;; It goes in an infinite loop
+;; Fix it so that it returns "frzzmlptay."
+(define (pigl wd)
+  (if (member? (first wd) 'aeiou)
+      (word wd 'ay)
+      (pigl (word (bf wd) (first wd)))))
+;; (pigl-r 'frzzmlpt "")
+(define (pigl-r the-word outp)
+  (display-all "calling pigl-r with the-word: " the-word ", outp: " outp)
+  (cond ((equal? (count the-word) 0) (word outp 'ay)) 
+        ((member? (first the-word) 'aeiou) (word the-word outp 'ay))
+        (else (pigl-r (butfirst the-word) (word outp (first the-word))))))
+;; I guess this is keep
+
+;; 14.14  Write a predicate same-shape? that takes two sentences as arguments. 
+;; It should return #t if two conditions are met: 
+;; The two sentences must have the same number of words, 
+;; and each word of the first sentence must have the same number of letters as the word in the corresponding position in the second sentence.
+;; > (same-shape? '(the fool on the hill) '(you like me too much))
+;; #T
+;; > (same-shape? '(the fool on the hill) '(and your bird can sing))
+;; #F
+;; > (same-shape? '(the fool on the hill) '(you like me too much) #f)
+;; > (same-shape? '(the fool on the hill) '(and your bird can sing) #f)
+;; not tail-recursive
+(define (same-shape? first-sent second-sent outp)
+  (display-all "calling same-shape? with first-sent: " first-sent ", second-sent: " second-sent ", outp: " outp)
+  (cond ((not (equal? (count first-sent) (count second-sent))) #f)
+        ((and (empty? first-sent) (empty? second-sent)) outp)
+        ((equal? (count (first first-sent)) (count (first second-sent))) (same-shape? (butfirst first-sent) (butfirst second-sent) #t))
+        (else #f)
+))
+;; This is accumulate
+
+;; 14.15  Write merge, a procedure that takes two sentences of numbers as arguments. 
+;; Each sentence must consist of numbers in increasing order. 
+;; Merge should return a single sentence containing all of the numbers, in order. 
+;; (We'll use this in the next chapter as part of a sorting algorithm.)
+;; > (merge '(4 7 18 40 99) '(3 6 9 12 24 36 50))
+;; (3 4 6 7 9 12 18 24 36 40 50 99)
+;; I will just assume that all of the numbers are already sorted
+;; And that there are no numbers in both lists
+;; Two into one: accumulate
+;; (merge-r '(4 7 18 40 99) '(3 6 9 12 24 36 50) '())
+;; not tail recursive, lots of conditions
+(define (merge-r nums-a nums-b outp)
+  (display-all "calling merge-r with nums-a: " nums-a ", nums-b: " nums-b ", outp: " outp)
+  (cond ((and (empty? nums-a) (empty? nums-b)) outp)
+        ((empty? nums-a) (sentence outp nums-b))
+        ((empty? nums-b) (sentence outp nums-a))
+        ((< (first nums-a) (first nums-b)) (merge-r (butfirst nums-a) nums-b (sentence outp (first nums-a))))
+        ((< (first nums-b) (first nums-a)) (merge-r nums-a (butfirst nums-b) (sentence outp (first nums-b))))
+        (else outp)))
+
+;; 14.16  Write a procedure syllables that takes a word as its argument and returns the number of syllables in the word, 
+;; counted according to the following rule: 
+;; the number of syllables is the number of vowels, 
+;; except that a group of consecutive vowels counts as one. 
+;; For example, in the word "soaring," 
+;; the group "oa" represents one syllable and the vowel "i" represents a second one.
+
+;; Be sure to choose test cases that expose likely failures of your procedure. 
+;; For example, what if the word ends with a vowel? 
+;; What if it ends with two vowels in a row? What if it has more than two consecutive vowels?
+
+;; Of course this rule isn't good enough. 
+;; It doesn't deal with things like silent "e"s that don't create a syllable ("like"), 
+;; consecutive vowels that don't form a diphthong ("cooperate"), 
+;; letters like "y" that are vowels only sometimes, etc. 
+;; If you get bored, see whether you can teach the program to recognize some of these special cases.
+;; collection to one answer: accumulate
+;; I think other languages would not have those problems.
+;; German: It's the same, every time.
+;; And Finn talked about Gaelic having long and short vowels, and they always follow a pattern.
+;; call like this: (syllables-r 'some-word 0)
+(define (syllables-r the-word outp)
+  (display-all "calling syllables-r with the-word: " the-word ", outp: " outp)
+  (cond ((or (empty? the-word) (equal? (count the-word) 1)) outp)
+        ((and (vowel? (first the-word)) (not (vowel? (first (butfirst the-word))))) (syllables-r (butfirst the-word) (+ 1 outp)))
+        ; ((and (not (vowel? (first the-word))) (vowel? (first (butfirst the-word)))) (syllables-r (butfirst the-word) (+ 1 outp)))
+        (else (syllables-r (butfirst the-word) outp))))
 
 
 
