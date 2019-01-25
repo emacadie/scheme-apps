4b566562116b227f8a83e24d6c1342b551b52490
Eric MacAdie <eric@MacAdie.net>
2019-01-25 09:27:37 -0600

    More chapter 15

--- tutorials/simply.scheme/chapter15.scm
+++ tutorials/simply.scheme/chapter15.scm
@@ -26,10 +26,36 @@
 (define (from-binary bin-num)
   (from-binary-r (trim-leading-zeros bin-num) 0 0))
 
-(define (to-binary dec-num)
-  (to-binary-r (trim-leading-zeros dec-num))
-)
+(define (closest-power-of-2 the-num outp)
+  ; (display-all "closest-power-of-2 with the-num: " the-num ", outp: " outp ", (expt 2 outp) is: " (expt 2 outp))
+  (cond ((> (expt 2 outp) the-num) (- outp 1))
+        ((equal? (expt 2 outp) the-num) outp)
+        (else (closest-power-of-2 the-num (+ 1 outp)))))
 
+; This actually gets a list of where the ones go
+; come up with a better name later
+(define (get-all-powers-of-2 the-num outp)
+  (display-all "get-all-powers-of-2 with the-num: " the-num ", outp: " outp)
+  (cond ((equal? the-num 0) 
+         (display-all "in first cond of get-all-powers-of-2")
+         outp)
+        ((equal? the-num 2) (sentence 1 outp))
+        (else
+         (display-all "in else for get-all-powers-of-2")
+         (get-all-powers-of-2 (- the-num (expt 2 (closest-power-of-2 the-num 0))) 
+                              (sentence (+ (closest-power-of-2 the-num 0) 1) outp)))))
+
+(define (to-binary-r power-list counter outp)
+  (display-all "in to-binary-r with power-list: " power-list ", counter: " counter ", outp: " outp)
+  (cond ((empty? power-list) outp)
+        ((equal? (first power-list) counter) (to-binary-r (butfirst power-list) (+ 1 counter) (sentence 1 outp)))
+        (else (to-binary-r power-list (+ 1 counter) (sentence 0 outp)))))
+;; call this
+(define (to-binary dec-num)
+  (to-binary-r (get-all-powers-of-2 (trim-leading-zeros dec-num) '()) 
+               1
+               '()))
+;; I think this works
 
 ;; 15.2  A "palindrome" is a sentence that reads the same backward as forward. Write a predicate palindrome? that takes a sentence as argument and decides whether it is a palindrome. For example:
 ;; > (palindrome? '(flee to me remote elf))
@@ -49,4 +75,5 @@
 
 (define (palindrome? the-sent)
   (palindrome-r (remove-spaces the-sent) #f))
+
 
--- tutorials/simply.scheme/more.functions.scm
+++ tutorials/simply.scheme/more.functions.scm
@@ -7,3 +7,10 @@
   (for-each display vs)
   (newline))
 
+(define (trim-leading-zeros the-num)
+  (if (not (equal? (first the-num) 0))
+      the-num
+      (trim-leading-zeros (butfirst the-num))
+)
+)
+
