b11f5ec3678756c4db64aedbc11c710da9837337
Eric MacAdie <eric@MacAdie.net>
2019-03-12 02:48:59 -0500

    Working on a re-do of chapter 14 numbers

--- tutorials/simply.scheme/chapter14_second._numbers.scm
+++ tutorials/simply.scheme/chapter14_second._numbers.scm
@@ -1,6 +1,32 @@
 ;; break a string into sets of 3
-(define (break-into-threes the-num)
-  (do-string-break (number->string the-num) '()))
+
+(define (exponent-helper the-ex the-num)
+  (display-all "calling exponent-helper with the-ex: " the-ex ", the-num: " the-num)
+  (cond ((equal? the-ex 0) the-num)
+        ((equal? the-ex 2) 'hundred)
+        ((equal? the-ex 3) 'thousand)
+        ((equal? the-ex 6) '(million))
+        ((equal? the-ex 9) '(billion))
+        ((equal? the-ex 12) '(trillion))))
+; "item" in Simply Scheme starts counting a list at "1", not "0" like C
+(define number-table '(one two three four five six seven eight nine zero))
+
+(define teen-table '(eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen))
+
+(define (get-teen-table the-num)
+  (if (equal? the-num 0) 
+      'ten
+      (item the-num teen-table)))
+
+(define power-1-table '(ten twenty thirty forty fifty sixty seventy eighty ninety))
+
+(define (power-1-helper the-num)
+  (item the-num power-1-table))
+
+(define (get-last-3 the-num)
+  (word (last (butlast (butlast the-num)))
+        (last (butlast the-num))
+        (last the-num)))
 
 (define (remove-leading-zeros num outp)
   (cond ((empty? num) outp) 
@@ -8,12 +34,36 @@
         (else num)))
 
 (define (do-string-break num-string num-list)
-  (display-all "do-string-break, num-string: " num-string ", num-list: " num-list)
   (cond ((<= (count num-string) 3)  (sentence num-string num-list))
         (else (do-string-break (butlast (butlast (butlast num-string)))
                                (sentence (remove-leading-zeros 
-                                          (word (last (butlast (butlast num-string))) (last (butlast num-string)) (last num-string)) 
+                                          (word (get-last-3 num-string)) 
                                           "" )
-                                         
                                          num-list)))))
+
+(define (get-new-teen-table n)
+  (display-all "get-new-teen-table: " n)
+  (cond ((empty? n) 'ten) 
+        ((equal? (first n) 'one) 'eleven)
+        ((equal? (first n) 'two) 'twelve)
+        ((equal? (first n) 'three) 'thirteen)
+        (else (word (first n) 'teen))))
+
+(define (num-name2-work num-work output depth)
+  (display-all "num-name2-work with num-work: " num-work ", output: " output ", depth: " depth)
+  (cond ((empty? num-work) output)
+        ;; skip ending zero
+        ((zero? (last num-work)) (num-name2-work (butlast num-work) output (+ 1 depth)))
+        ((equal? depth 1) (num-name2-work (butlast num-work) (sentence (item (last num-work) number-table)) (+ 1 depth)))
+        ; new teens
+        ((and (equal? depth 2) (equal? 1 (last num-work))) (num-name2-work (butlast num-work) (get-new-teen-table output) (+ 1 depth)))
+        ((equal? depth 2) (num-name2-work (butlast num-work) (sentence (item (last num-work) power-1-table)  output) (+ 1 depth)))
+        ((equal? depth 3) (num-name2-work (butlast num-work) (sentence (item (last num-work) number-table) 'hundred output) (+ 1 depth)))))
+
+(define (number-name-2 number)
+  (num-name2-work (last (do-string-break (number->string number) '())) 
+                  "" 
+                  1)
+)
+; break into threes
 
