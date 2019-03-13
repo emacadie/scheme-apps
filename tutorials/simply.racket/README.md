d22de00e988d0357cca668409d2f69d3724b9aec
Eric MacAdie <eric@MacAdie.net>
2019-03-13 02:55:21 -0500

    Simply Racket: Done with chapter 4

--- tutorials/simply.racket/README.md
+++ tutorials/simply.racket/README.md
@@ -4,51 +4,59 @@ A big part of the reason is that I want to be able to run some tests.
 I can run tests in Clojure, but Scheme is not as good in this area.  
 
 
-https://github.com/greghendershott/racket-mode
-https://melpa.org/#/racket-mode
+https://github.com/greghendershott/racket-mode  
+https://melpa.org/#/racket-mode  
 
-https://docs.racket-lang.org/manual@simply-scheme/index.html
-https://pkgs.racket-lang.org/package/simply-scheme
+https://docs.racket-lang.org/manual@simply-scheme/index.html  
+https://pkgs.racket-lang.org/package/simply-scheme  
+
+Racket reference:  
+https://docs.racket-lang.org/reference/index.html   
+
+Racket Cheat Sheet:   
+https://docs.racket-lang.org/racket-cheat/index.html   
+
+Racket Unit Testing:  
+https://docs.racket-lang.org/rackunit/index.html  
+
+raco pkg install drracket  
+ericm@latitude:~/Downloads$ raco pkg show  
+Installation-wide:  
+ Package            Checksum             Source  
+ main-distribution  59e9a33d2c5f01ae...  catalog...tribution  
+ racket-lib         cd090dc3c3452a44...  catalog racket-lib  
+ [188 auto-installed packages not shown]  
+User-specific for installation "6.9":  
+ Package                     Checksum          Source  
+ planet-dyoo-simply-scheme2  ce6e47b87642f...  catalog...cheme2  
+ simply-scheme               8b8ba2b50d868...  catalog...scheme  
+
+I uninstalled the simply-scheme, and I think I installed it with this:  
+raco pkg install simply-scheme  
+Either that or I did it in Dr Racket. Don't remember.  
+Using DrRacket version 6.9.  
+
+https://www.hashcollision.org/simply-scheme/  
 
 
-raco pkg install drracket
-ericm@latitude:~/Downloads$ raco pkg show
-Installation-wide:
- Package            Checksum             Source
- main-distribution  59e9a33d2c5f01ae...  catalog...tribution
- racket-lib         cd090dc3c3452a44...  catalog racket-lib
- [188 auto-installed packages not shown]
-User-specific for installation "6.9":
- Package                     Checksum          Source
- planet-dyoo-simply-scheme2  ce6e47b87642f...  catalog...cheme2
- simply-scheme               8b8ba2b50d868...  catalog...scheme
 
-I uninstalled the simply-scheme, and I think I installed it with this:
-raco pkg install simply-scheme
-Either that or I did it in Dr Racket. Don't remember.
-Using DrRacket version 6.9.
-
-https://www.hashcollision.org/simply-scheme/
-
-
-
-Emacs commands: racket-run
+Emacs commands: racket-run  
 https://github.com/greghendershott/racket-mode/blob/master/Reference.md   
 
-What finally installed it:
-in init.el:
-Adding the URL for MELPA
-Adding "racket-mode" to the list in (defvar my-packages
-Adding 
-;; Racket
-(setq racket-program "racket")
-and adding "racket-mode" to
-'(package-selected-packages
-   (quote
-at the end. Then emacs downloaded it all for me.
+What finally installed it:  
+in init.el:  
+Adding the URL for MELPA  
+Adding "racket-mode" to the list in (defvar my-packages  
+Adding   
+;; Racket  
+(setq racket-program "racket")  
+and adding "racket-mode" to  
+'(package-selected-packages  
+   (quote  
+at the end. Then emacs downloaded it all for me.  
 
-You may have to M-x racket-mode as well.
-To get repl: M-x racket-run
-To run the tests:
-M-x racket-test
+You may have to M-x racket-mode as well.  
+To get repl: M-x racket-run  
+To run the tests:  
+M-x racket-test  
 
--- tutorials/simply.racket/chapter04.rkt
+++ tutorials/simply.racket/chapter04.rkt
@@ -1,22 +1,95 @@
 #lang simply-scheme
+
 (se (butlast (bf "this"))
     "world")
 
 (define (sphere-volume r)
   (* (/ 4 3) 3.141592654 (* r r r))) 
 
-;; 4.5: Write a procedure to convert a temperature from Fahrenheit to Celsius, and another to convert in the other direction. The two formulas are F=9⁄5C+32 and C=5⁄9(F-32).  
+;; 4.5: Write a procedure to convert a temperature from Fahrenheit to Celsius, 
+;; and another to convert in the other direction. 
+;; The two formulas are F=9⁄5C+32 and C=5⁄9(F-32).  
 (define (c-to-f c)
-  (inexact (+ 32 (* (/ 9 5) c))))
+  (exact->inexact (+ 32 (* (/ 9 5) c))))
 
 (define (f-to-c f)
-  (* (/ 5 9) (- f 32)))
+  (exact->inexact (* (/ 5 9) (- f 32))))
+
+;; 4.6: Define a procedure fourth that computes the fourth power of its argument. 
+;; Do this two ways, first using the multiplication function, and then using square and not (directly) using multiplication. 
+(define (fourth x)
+    (* x (* x (* x x))))
+
+(define (square x)
+  (* x x))
+
+(define (fourth-with-square x)
+  (square (square x)))
+
+;; 4.7: Write a procedure that computes the absolute value of its argument by finding the square root of the square of the argument.
+(define (simply-abs x)
+  (sqrt (square x)))
+
+;; 4.8  "Scientific notation" is a way to represent very small or very large numbers by combining a medium-sized number with a power of 10. 
+;; For example, 5×107 represents the number 50000000, while 3.26×10-9 represents 0.00000000326 in scientific notation. 
+;; Write a procedure scientific that takes two arguments, a number and an exponent of 10, and returns the corresponding value:
+;; > (scientific 7 3)
+;; 7000
+;; > (scientific 42 -5)
+;; 0.00042
+;; Some versions of Scheme represent fractions in a/b form, and some use scientific notation, 
+;; so you might see 21/50000 or 4.2E-4 as the result of the last example instead of 0.00042, but these are the same value.
+(define (scientific num exp)
+  (exact->inexact (* num (expt 10 exp))))
+
+;; 4.9  Define a procedure discount that takes two arguments: an item's initial price and a percentage discount. It should return the new price:
+;; > (discount 10 5)
+;; 9.50
+;; > (discount 29.90 50)
+;; 14.95
+(define (discount initial percent)
+  (* initial (exact->inexact (/ (- 100 percent ) 100))))
+
+;;  4.10  Write a procedure to compute the tip you should leave at a restaurant. 
+;; It should take the total bill as its argument and return the amount of the tip. 
+;; It should tip by 15%, but it should know to round up so that the total amount of money you leave (tip plus original bill) is a whole number of dollars. 
+;; (Use the ceiling procedure to round up.)
+
+;; > (tip 19.98)
+;; 3.02 - for some reason I get 3.0199999999999996
+;; > (tip 29.23)
+;; 4.77
+;; > (tip 7.54)
+;; 1.46
+;; 
+(define (tip total-bill)
+  (- (ceiling (* total-bill 1.15)) total-bill))
 
 (module+ test
   (require rackunit)
   (check-true #t)
   (printf "(sphere-volume 10):  ~a \n" (sphere-volume 10)) 
   (check-equal? (sphere-volume 10) 4188.790205333334 "Wrong answer for sphere-volume" ) 
+  (printf "(c-to-f 23): ~a \n" (c-to-f 23))
+  (check-equal? (c-to-f 23) 73.4 "Wrong answer for (c-to-f 23)")
+  (printf "(f-to-c 55): ~a \n" (f-to-c 55))
+  (check-equal? (f-to-c 55) 12.777777777777779 "Wrong answer for (f-to-c 55)")
+  (printf "(fourth-with-square 4): ~a \n" (fourth-with-square 4))
+  (check-equal? (fourth-with-square 4) 256 "Wrong answer for (fourth-with-square 4)")
+  (printf "(simply-abs -4): ~a \n" (simply-abs -4))
+  (check-equal? (simply-abs -4) 4 "Wrong answer for (simply-abs -4)")
+  (printf "(scientific 7 3): ~a \n" (scientific 7 3))
+  (check-equal? (scientific 7 3) 7000.0 "Wrong answer for (scientific 7 3)")
+  (printf "(scientific 42 -5): ~a \n" (scientific 42 -5))
+  (check-equal? (scientific 42 -5) 0.00042 "Wrong answer for (scientific 42 -5)")
+  (printf "(discount 10 5): ~a \n" (discount 10 5))
+  (check-equal? (discount 10 5) 9.5 "Wrong answer for (discount 10 5)")
+  (printf "(discount 29.90 50): ~a \n" (discount 29.90 50))
+  (check-equal? (discount 29.90 50) 14.95 "Wrong answer for (discount 29.90 50)")
+  (printf "(tip 29.23): ~a \n" (tip 29.23))
+  (check-equal? (tip 29.23) 4.77 "Wrong answer for (tip 29.23)")
+  (printf "(tip 7.54): ~a \n" (tip 7.54))
+  (check-equal? (tip 7.54) 1.46 "Wrong answer for (tip 7.54)")
 )
 
 
