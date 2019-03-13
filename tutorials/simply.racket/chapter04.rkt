2c9cba777728f48c6bf36af9e7fd4a01be6ceee3
Eric MacAdie <eric@MacAdie.net>
2019-03-13 01:13:24 -0500

    Working on chapter04 again

--- tutorials/simply.racket/chapter04.rkt
+++ tutorials/simply.racket/chapter04.rkt
@@ -5,6 +5,13 @@
 (define (sphere-volume r)
   (* (/ 4 3) 3.141592654 (* r r r))) 
 
+;; 4.5: Write a procedure to convert a temperature from Fahrenheit to Celsius, and another to convert in the other direction. The two formulas are F=9⁄5C+32 and C=5⁄9(F-32).  
+(define (c-to-f c)
+  (inexact (+ 32 (* (/ 9 5) c))))
+
+(define (f-to-c f)
+  (* (/ 5 9) (- f 32)))
+
 (module+ test
   (require rackunit)
   (check-true #t)
