eebf1eee545c4f13a89fd9beb131f8bb46607269
Eric MacAdie <eric@MacAdie.net>
2019-05-30 21:45:24 -0500

    Working on chapter 17

--- tutorials/simply.racket/chapter17.rkt
+++ tutorials/simply.racket/chapter17.rkt
@@ -32,7 +32,56 @@ So assoc takes a key and a key/value list, and returns the element with
 the key and the value, else returns #f
 Totally different than Clojure's assoc.
 
+
+Concepts introduced in this chapter:
+selectors and constructors
+list, cons, append
+car and cdr
+list?, equal?, member?, list-ref, length, assoc
+functions taking varying numbers of args
+apply function: "It takes two arguments, a procedure and a list"
+"Recursion on Arbitrary Structured Lists"
+Searching for an element in a multi-level list
+Using higher-order functions:
+(define (deep-appearances wd structure)
+  (if (word? structure)
+      (if (equal? structure wd) 
+          1 
+          0)
+      (reduce +
+	      (map (lambda (sublist) (deep-appearances wd sublist))
+		   structure))))
+Using standard recursion (three base cases and two recursive calls)
+(define (deep-appearances wd structure)
+  (cond ((equal? wd structure) 1)              ; base case: desired word
+        ((word? structure) 0)                  ; base case: other word
+        ((null? structure) 0)                  ; base case: empty list
+        (else (+ (deep-appearances wd (car structure))
+                 (deep-appearances wd (cdr structure))))))
+I don't think you can do tail-recursion for this stuff.
 |#
+
+;; 17.2  For each of the following examples, write a procedure of two arguments that, 
+;; when applied to the sample arguments, returns the sample result. 
+;; Your procedures may not include any quoted data.
+; > (f1 '(a b c) '(d e f))
+; ((B C D))
+(define (f1 list1 list2)
+  (list (append (cdr list1) (car list2))))
+
+;> (f2 '(a b c) '(d e f))
+;((B C) E)
+(define (f2 list1 list2)
+  (cons (cdr list1) (car (cdr list2))))
+;> (f3 '(a b c) '(d e f))
+;(A B C A B C)
+(define (f3 list1 list2)
+  (append list1 list1))
+;> (f4 '(a b c) '(d e f))
+;((A D) (B C E F))
+(define (f4 list1 list2)
+  (list (list (car list1) (car list2))
+        (append (cdr list1) (cdr list2))))
 
 ;; 17.4  Describe the result of calling the following procedure with a list as its argument. 
 ;; (See if you can figure it out before you try it.) 
@@ -45,9 +94,84 @@ Totally different than Clojure's assoc.
       other
       (mystery-helper (cdr lst) (cons (car lst) other))))
 
+;;  17.5  Here's a procedure that takes two numbers as arguments and returns whichever number is larger:
+(define (max2 a b)
+  (if (> b a) 
+      b 
+      a))
+
+#|
+; application: not a procedure;
+;  expected a procedure that can be applied to arguments
+;   given: '(2 4)
+;   arguments...: [none]
+I had "rest-of-nums" in parens by mistake
+|#
+
+;; Use max2 to implement max, 
+;; a procedure that takes one or more numeric arguments and returns the largest of them. 
+(define (my-max number . rest-of-nums)
+  (printf "-- Calling my-max with number: ~a and rest-of-nums: ~a" number rest-of-nums)
+  (printf " count of rest-of-nums is: ~a \n" (count rest-of-nums))
+  (printf "(car rest-of-nums) is ~a \n" (car rest-of-nums))
+  (printf "Here is (max2 number (car rest-of-nums)): ~a\n" (max2 number (car rest-of-nums)))
+  (cond [(null? rest-of-nums) number]
+        ; [(= (count rest-of-nums) 1) (apply max2 (append number (car rest-of-nums)) )]
+        [(= (count rest-of-nums) 1) (begin
+                                      (printf "count of rest-of-nums is 1\n")
+                                      ; (apply max2 (append number (car rest-of-nums)) )
+                                      (max2 number (car rest-of-nums))
+                                      )] ; okay
+        ; [(= (count rest-of-nums) 1) (printf "(count rest-of-nums) is 1\n")]
+        [(= (max2 number (car rest-of-nums)) number) 
+         ; (= number (max2 number (car rest-of-nums))) 
+         (begin (printf "(max2 ~a (car ~a)) is: ~a\n" number rest-of-nums (max2 number (car rest-of-nums)))
+                (printf "here is (cdr rest-of-nums): ~a \n" (cdr rest-of-nums))
+                ; (my-max number (cdr rest-of-nums))
+                (apply my-max (list number (cdr rest-of-nums)))
+           )]
+        ; [(= (car rest-of-nums) (max2 number (car rest-of-nums))) (my-max (car rest-of-nums) (cdr rest-of-nums))]
+        ; [(= (car rest-of-nums) (max2 number (car rest-of-nums))) (apply my-max rest-of-nums)]
+        [(= (car rest-of-nums) (max2 number (car rest-of-nums))) ;
+         (begin
+           (printf "(car rest-of-nums) is the result of (max2 number (car rest-of-nums))\n" )
+           (apply my-max rest-of-nums)
+)] ;; okay
+        [else number]
+         
+  )
+)
+
+
+
+(define (my-max2 number . more-nums)
+  (printf "Hello: number is: ~a\n" number)
+  (printf "Here is more-nums: ~a\n" more-nums)
+  (cond [(null? more-nums) number]
+)
+)
+
+
+
+(define (increasing? number . rest-of-numbers)
+  (cond ((null? rest-of-numbers) #t)
+	((> (car rest-of-numbers) number)
+	 (apply increasing? rest-of-numbers))
+	(else #f)))
+
 (module+ test
   (require rackunit)
   (check-true #t)
+  ;; they said the dot is not part of the actual list, so I guess this is okay
+  (printf "(f1 '(a b c) '(d e f)): ~a \n" (f1 '(a b c) '(d e f)))
+  (check-equal? (f1 '(a b c) '(d e f)) '((b c . d)) "Error for (f1 '(a b c) '(d e f))")
+  (printf "(f2 '(a b c) '(d e f)): ~a \n" (f2 '(a b c) '(d e f)))
+  (check-equal? (f2 '(a b c) '(d e f)) '((b c) . e) "Error for (f2 '(a b c) '(d e f))")
+  (printf "(f3 '(a b c) '(d e f)): ~a \n" (f3 '(a b c) '(d e f)))
+  (check-equal? (f3 '(a b c) '(d e f)) '(a b c a b c) "Error for (f3 '(a b c) '(d e f))")
+  (printf "(f4 '(a b c) '(d e f)): ~a \n" (f4 '(a b c) '(d e f)))
+  (check-equal? (f4 '(a b c) '(d e f)) '((a d) (b c e f)) "Error for (f4 '(a b c) '(d e f))")
+
   ; (printf "(who '(sells out)): ~a \n" (who '(sells out)))
   ; (check-equal? (who '(sells out)) '(pete sells out roger sells out john sells out keith sells out) "Error for (who '(sells out))")
   (printf "(mystery '(1 2 3 4)): ~a \n" (mystery '(1 2 3 4)))
