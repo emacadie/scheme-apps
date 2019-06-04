ba7afe1f40942c303956e27e99e5d826f1373289
Eric MacAdie <eric@MacAdie.net>
2019-06-03 22:05:42 -0500

    Working some more on chapter 17 problem 5

--- tutorials/simply.racket/chapter17_05.backup.rkt
+++ tutorials/simply.racket/chapter17_05.backup.rkt
@@ -28,7 +28,7 @@ I had "rest-of-nums" in parens by mistake
 ;; If I get to the last element in the list, it sends it as a list to max2, not as a number.
 ;; I have been unable to figure it out.
 ;; ** A minute later ** -> caar seems to work
-
+#|
 (define (my-max number . rest-of-nums)
   (cond [(null? rest-of-nums) number]
         [(and (= (count rest-of-nums) 1) (number? (car rest-of-nums))) (max2 number (car rest-of-nums))]
@@ -36,13 +36,13 @@ I had "rest-of-nums" in parens by mistake
         [(= (max2 number (car rest-of-nums)) number) (my-max number (append '() (cdr rest-of-nums)))]
         [(= (max2 number (car rest-of-nums)) (car rest-of-nums)) (apply my-max rest-of-nums)] ;; okay
         [else number]))
-
+|#
 ; this seems to work
-(define (reduce-max number . more-nums)
-   (reduce max2 (append (list number) more-nums)))
+;; (define (reduce-max number . more-nums)
+;;   (reduce max2 (append (list number) more-nums)))
 
 ; this one is not working out as well as I had hoped
-
+#|
 (define (simply-max number . more-nums)
   (cond [(null? more-nums) number]
         ;; after it got through the list of nums, Racket sent an empty list inside a list: '(())
@@ -51,16 +51,16 @@ I had "rest-of-nums" in parens by mistake
         [(= (first more-nums) (max2 number (first more-nums))) ; (apply simply-max more-nums)
          (simply-max (first more-nums) (butfirst more-nums))]
         [else number]))
-
+|#
 ;; I admit, I looked at some of the other answers for this one.
 ;; Use max2 to create a list that apply sends to the function
-
+#|
 (define (split-max number . more-nums)
   (cond [(null? more-nums) number]
         [else (apply split-max (cons (max2 number (car more-nums)) (cdr more-nums)))] ))
-
+  |#
 ;; line 150
-
+#|
 (module+ test
   (require rackunit)
   (check-true #t)
@@ -72,7 +72,22 @@ I had "rest-of-nums" in parens by mistake
                  (check-equal? answer split-max-a))
       (fail-check)))
 
-    
-) ;; end module+ test 
+  (check-maxes-equal? 4 (my-max 1 2 3 4) (reduce-max 1 2 3 4) (simply-max 1 2 3 4) (split-max 1 2 3 4))
+  (printf "If the maxes are equal, then (my-max 1 2 3 4) is ~a\n" (reduce-max 1 2 3 4))
+  
+  (check-maxes-equal? 4 (my-max 1 2 4 3 4) (reduce-max 1 2 4 3 4) (simply-max 1 2 4 3 4) (split-max 1 2 4 3 4))
+  (printf "If the maxes are equal, then (my-max 1 2 4 3 4) is ~a\n" (reduce-max 1 2 4 3 4))
 
+  (check-maxes-equal? 4 (my-max 4 3 2 1) (reduce-max 4 3 2 1) (simply-max 4 3 2 1) (split-max 4 3 2 1))
+  (printf "If the maxes are equal, then (my-max 4 3 2 1) is ~a\n" (reduce-max 4 3 2 1))
+
+  (check-maxes-equal? 4 (my-max 4 3 4 2 1) (reduce-max 4 3 4 2 1) (simply-max 4 3 4 2 1) (split-max 4 3 4 2 1))
+  (printf "If the maxes are equal, then (my-max 4 3 4 2 1) is ~a\n" (reduce-max 4 3 4 2 1))
+  ; 
+  (check-maxes-equal? 5 (my-max 4 3 5 2 1) (reduce-max 4 3 5 2 1) (simply-max 4 3 5 2 1) (split-max  4 3 5 2 1)
+  (printf "If the maxes are equal, then (my-max 4 3 5 2 1) is ~a\n" (reduce-max 4 3 5 2 1))
+
+  
+) ;; end module+ test 
+  |#
 
