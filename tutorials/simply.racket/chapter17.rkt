ebb396c2d68e444347f8b6a482cb3ce09a5a220e
Eric MacAdie <eric@MacAdie.net>
2019-06-08 23:37:12 -0500

    Working on 17.7

--- tutorials/simply.racket/chapter17.rkt
+++ tutorials/simply.racket/chapter17.rkt
@@ -109,6 +109,7 @@ I don't think you can do tail-recursion for this stuff.
 ;; but not tail-recursive (and you know I love tail-recursive)
 ;; I am not handling pairs quite right, but I will go with it
 (define (my-append listA listB)
+  ; (printf "Calling my-append with listA: ~a and listB: ~a \n" listA listB)
   (cond [(or (null? listB) (empty? listB)) listA]
         [(or (null? listA) (empty? listA)) listB]
         [(and (not (list? listB)) (not (pair? listB))) 
@@ -125,12 +126,85 @@ I don't think you can do tail-recursion for this stuff.
            )
               ]))
 
+;; again, not perfect, does not handle pairs too well, but I think I will take it
+;; Isn't a pair just a list anyway?
+;; According to https://stackoverflow.com/questions/6006671/are-pair-and-list-different-in-scheme
+;; they are different
+;; But the book said they are pretty much the same
+(define (append-multi-lists listA . listB)
+  ; (printf "-- calling append-multi-lists with listA: ~a and listB: ~a\n" listA listB)
+  (cond [(or (null? listB) (empty? listB)) listA]
+        [(or (null? listA) (empty? listA)) (apply append-multi-lists listB)] ;; could this be done with car and cdr?
+        ; [(or (null? listA) (empty? listA)) (append-multi-lists (car listB) (cdr listB))] ;; could this be done with car and cdr?
+        [(and (= (count listB) 1 ) (list? (car listB)) (empty? (car listB))) listA]
+        [else
+         (begin
+           ; (printf "In the else, car listB: ~a cdr listB: ~a\n" (car listB) (cdr listB))
+           ; (append-multi-lists (my-append listA (car listB)) (cdr listB))
+           (apply append-multi-lists (my-append listA (car listB)) (cdr listB))
+           )]))
 
+;; This works with lists, but not if some of the items are solo numbers
+(define (append-m-lists-reduce listA . listB)
+  (printf "calling append-m-lists-reduce with listA: ~a and listB: ~a\n" listA listB)
+  (printf "here is our appended lists: ~a\n" (append '() listA listB))
+  (my-append listA (reduce my-append listB))
+)
 
+;; from https://github.com/pongsh/simply-scheme-exercises/blob/master/17-lists/17.6.scm
 (define (their-append lst1 lst2)
   (if (null? lst1)
       lst2
       (cons (car lst1) (their-append (cdr lst1) lst2))))
+(define (their-multi-append lst . rest-of-list )
+  (if (null? rest-of-list)
+      lst
+      (apply their-multi-append (cons (their-append lst (car rest-of-list)) 
+                                      (cdr rest-of-list)))))
+
+;; 17.7  Append may remind you of sentence. 
+;; They're similar, except that append works only with lists as arguments, whereas sentence will accept words as well as lists. 
+;; Implement sentence using append. 
+;; (Note: The built-in sentence can take any number of arguments. 
+;; First write a version that accepts only two arguments. 
+;; Then, optionally, try to write a version that takes any number. 
+;; Also, you don't have to worry about the error checking that the real sentence does.) 
+;; Also from text:
+;; How does our sentence point of view differ from the built-in Scheme point of view using lists? There are three differences:
+;; A sentence can contain only words, not sublists.
+;; Sentence selectors are symmetrical front-to-back.
+;; Sentences and words have the same selectors.
+
+;; I will still check that if it's a list it does not have a sublist
+(define (list-with-sublists the-list)
+  (cond [(not (list? the-list)) #f]
+        [(= 0 (keep list? the-list)) #f]
+        [else #t])) 
+
+
+(define (list-or-item the-item)
+  (cond [(not (list? the-item)) (list the-item)]
+        [(list-with-sublists the-item) null]
+        [else the-item]))
+
+
+#|
+(append (list  'hello) '(this is a ren))
+'(hello this is a ren)
+chapter17.rkt/test> (append '(what is this) '(this is a ren))
+'(what is this this is a ren)
+chapter17.rkt/test> (append '(what is this) 're)
+'(what is this . re)
+chapter17.rkt/test> (append '(what is this) (list 're))
+'(what is this re)
+|#
+
+(define (sent-via-append list-a list-b)
+  (append (list-or-item list-a) (list-or-item list-b))
+)
+
+
+;; 
 
 (module+ test
   (require rackunit)
