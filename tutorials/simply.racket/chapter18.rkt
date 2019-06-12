c2b12313ff20740028181fb00f4171979ff01cf3
Eric MacAdie <eric@MacAdie.net>
2019-06-12 08:53:42 -0500

    Done with chapter 17, starting chapter 18

--- tutorials/simply.racket/chapter18.rkt
+++ tutorials/simply.racket/chapter18.rkt
@@ -1,66 +1,24 @@
 #lang simply-scheme
 
-; Chapter 17: Lists
+; Chapter 18: Lists
 
 (require "more-simply.rkt")
 
-(butfirst '(This is chapter 17 lists))
+(butfirst '(This is chapter 18 trees))
 
-;; Chapter 17 Lists
-;; for procedures like cadar and cddadr, read the a's and d's from right to left
+;; Chapter 18 Trees
+
 
 #|
-From the text:
-It's important that you understand how list, cons, and append differ from each other:
+Concepts in this chapter:
+Trees: root node, branch node, leaf node
+Each node has a datum (word or sentence), and zero or more children.
+Trees are recursive: Trees have trees in them, man. Dave's not here. Mind blown.
+Nodes can be parents, siblings, children
 
-> (list '(i am) '(the walrus))
-((I AM) (THE WALRUS))
-
-> (cons '(i am) '(the walrus))
-((I AM) THE WALRUS)
-
-> (append '(i am) '(the walrus))
-(I AM THE WALRUS)
-so list will combine elements as lists
-cons will flatten the second arg
-cons is like conj for lists in Clojure
-append flattens all
-append takes a list and what you are adding to it
-cons adds first arg to front of list (which is second arg)
-(assoc 'Colin '((Rod Argent) (Chris White)
-		  (Colin Blunstone) (Hugh Grundy) (Paul Atkinson)))
-returned '(Colin Blunstone)
-So assoc takes a key and a key/value list, and returns the element with 
-the key and the value, else returns #f
-Totally different than Clojure's assoc.
-
-
-Concepts introduced in this chapter:
-selectors and constructors
-list, cons, append
-car and cdr
-list?, equal?, member?, list-ref, length, assoc
-functions taking varying numbers of args
-apply function: "It takes two arguments, a procedure and a list"
-"Recursion on Arbitrary Structured Lists"
-Searching for an element in a multi-level list
-Using higher-order functions:
-(define (deep-appearances wd structure)
-  (if (word? structure)
-      (if (equal? structure wd) 
-          1 
-          0)
-      (reduce +
-	      (map (lambda (sublist) (deep-appearances wd sublist))
-		   structure))))
-Using standard recursion (three base cases and two recursive calls)
-(define (deep-appearances wd structure)
-  (cond ((equal? wd structure) 1)              ; base case: desired word
-        ((word? structure) 0)                  ; base case: other word
-        ((null? structure) 0)                  ; base case: empty list
-        (else (+ (deep-appearances wd (car structure))
-                 (deep-appearances wd (cdr structure))))))
-I don't think you can do tail-recursion for this stuff.
+Mutual recursion
+initialization procedure calls its helper procedure
+helper procedure calls itself and the initialization procedure
 |#
 
 ;; 17.2  For each of the following examples, write a procedure of two arguments that, 
@@ -329,7 +287,7 @@ I don't think you can do tail-recursion for this stuff.
         [else (valid-infix-helper (cdr op-list) (+ 1 position))]))
 
 (define (valid-infix? op-list)
-  (cond [(even? (count (flatten-reduce op-list))) #f]
+  (cond [(not (valid-infix-easy? op-list)) #f]
         [else (valid-infix-helper (flatten-reduce op-list) 1)]))
 
 ;; filter out the odds and the evens, and make sure all the odds are numbers, and there is one more odd than even
